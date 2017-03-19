4a3f620fa9cb687255c7022fd0929d481461dbc5
IsoaSFlus <me@isoasflus.me>
2017-03-19 09:16:47 +0800

    script improved

--- yande.py
+++ yande.py
@@ -5,16 +5,16 @@ import re
 import random
 import subprocess
 import time
+import sys
 
 url_params = {'tags' : 'landscape order:random'}
 yande_url = 'https://yande.re/post'
 download_dir = '/home/midorikawa/图片/yande_wallpaper/'
-fit_flag = 0
 
 def is_resolution_fit(photo_resolution):
     res = re.match('(\S+) x (\S+)', photo_resolution)
     print(res.group(0))
-    if int(res.group(1)) > 1600 and int(res.group(2)) > 900 and int(res.group(1)) > int(res.group(2)):
+    if int(res.group(1)) > 1600 and int(res.group(2)) > 900 and (int(res.group(1)) / int(res.group(2))) > 1.5 and (int(res.group(1)) / int(res.group(2))) < 2.1:
         return True
     else:
         return False
@@ -37,9 +37,16 @@ def main():
     file_name = 'wallpaper-' + '%02d%02d%02d'%(time_stamp.tm_hour, time_stamp.tm_min, time_stamp.tm_sec) + '.jpg'
 
     random_num = random.randint(0,39)
-    
-#    subprocess.call(["gsettings", "set", "org.gnome.desktop.background", "picture-uri", "file://" + download_dir + file_name])
-    page_html = requests.get(yande_url, params = url_params)
+
+    try:
+        page_html = requests.get(yande_url, params = url_params)
+    except Exception as e:
+        print('Failed, try again.')
+        try:
+            page_html = requests.get(yande_url, params = url_params)
+        except Exception as er:
+            print('Unfortunately')
+            sys.exit()
 
     photo_url_list = get_photos_url_list(page_html.text)
     photo_res_list = get_photos_resolution_list(page_html.text)
@@ -49,12 +56,13 @@ def main():
         if is_resolution_fit(photo_res_list[random_num]):
             fit_flag = 1
             cmd = 'wget -O ' + download_dir + file_name + ' %s'%photo_url_list[random_num]
-            subprocess.call(cmd, shell = True)
-#            download_photo('%s'%photo_url_list[random_num])
-#            subprocess.call('mv ' + download_dir + 'wallpaper.jpg ' + download_dir + time_stamp + 'wallpaper_done.jpg', shell = True)
+            try:
+                subprocess.call(cmd, shell = True)
+            except Exception as e:
+                print(e)
+                fit_flag = 0
 
     subprocess.call('PID=$(pgrep gnome-session) && export DBUS_SESSION_BUS_ADDRESS=$(grep -z DBUS_SESSION_BUS_ADDRESS /proc/$PID/environ|cut -d= -f2-) && gsettings set org.gnome.desktop.background picture-uri "file://' + download_dir + file_name + '"', shell = True)
-    
 
 
 if __name__ == '__main__':
